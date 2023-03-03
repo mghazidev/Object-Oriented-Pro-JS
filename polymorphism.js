22999300fdc536b9210c0011b27682643d4507e6
mghazidev <mghazidev@gmail.com>
2023-03-03 13:19:10 +0500

    Many-Forms

---
+++ polymorphism.js
@@ -0,0 +1,42 @@
+class characters {
+    constructor(name,health,attack) {
+        this.name = name;
+        this.health = health;
+        this.attack = attack;
+    }
+    
+    target(Target) {
+        console.log(`${this.name} attacks ${this.name} with ${this.attact}`)
+    }
+
+}
+
+class wizards extends characters {
+    constructor(name,health,attack,spell) {
+        super(name,health,attack);
+        this.spell = spell;
+    }
+    target(Target) {
+        console.log(`${this.name} cast ${this.spell} to ${Target.name}}`)    
+    }
+}
+
+class warriors extends characters {
+    constructor(name,health,attack,weapons) {
+        super(name,health,attack);
+        this.weapons = weapons;
+    }
+    target(Target) {
+        console.log(`${this.name} attacks ${Target.name} with ${this.weapons}`)
+    }
+}
+
+let Wizard = new wizards("King","100%","andar","Sword")
+let Warrior = new warriors("queen","90%","Hands","Fireball")
+
+Wizard.target(Warrior)
+Warrior.target(Wizard)
+
+
+
+//In this example we are using polimorphism to give the different act to each hero. In short we are doing different things in the same object using polymorphism
